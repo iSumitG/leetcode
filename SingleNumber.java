865b38f7e835d954a9699c4797a1418f27438a7a
Sumit Gupta <sumit743@gmail.com>
2016-07-20 18:45:32 -0400

    Create SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,12 @@
+// Solution to 136. Single Number
+// Problem: https://leetcode.com/problems/single-number/
+// Author: Sumit Gupta
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int i=0; i<nums.length; i++)
+            result ^= nums[i];
+        return result;
+    }
+}
