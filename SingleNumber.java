e29ad4088dcd68493e75ddfd2d3ef5532d757b8b
Sumit Gupta <sumit743@gmail.com>
2016-07-23 23:53:04 -0400

    Update SingleNumber.java

--- SingleNumber.java
+++ SingleNumber.java
@@ -5,7 +5,6 @@
 public class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
-        
         for(int i=0; i<nums.length; i++)
             result ^= nums[i];
         
