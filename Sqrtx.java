9dcd571897f0742ee73aa55e37cef7d627974e6b
Sumit Gupta <sumit743@gmail.com>
2016-07-24 02:32:42 -0400

    Update Sqrtx.java

--- Sqrtx.java
+++ Sqrtx.java
@@ -3,26 +3,26 @@
 // Author: Sumit Gupta
 
 public class Solution {
-	public int mySqrt(int x) {
-		if (x <= 1)
-			return x;
-		int left = 0, right = x;
-
-		while (left < right) {
-			int mid = left + (right - left) / 2;
-
-			if (mid == x / mid)
-				return mid;
-
-			if (mid < x / mid) {
-				if (mid + 1 > x / (mid + 1))
-					return mid;
-				left = mid + 1;
-			}
-			else
-				right = mid - 1;
-		}
-
-		return left;
-	}
+    public int mySqrt(int x) {
+        if(x <= 1) return x;
+        
+        int left = 0, right = x;
+        
+        while(left < right) {
+            int mid = left + (right-left)/2;
+            
+            if(mid == x/mid)
+                return mid;
+            
+            if(mid < x/mid) {
+                if(mid+1 > x/(mid+1))
+                    return mid;
+                left = mid+1;
+            }
+            else
+                right = mid-1;
+        }
+        
+        return left;
+    }
 }
