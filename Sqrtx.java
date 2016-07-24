8afc687012827d61e7f56e99cc8e1caa852be364
Sumit Gupta <sumit743@gmail.com>
2016-07-24 02:26:00 -0400

    Create Sqrtx.java

---
+++ Sqrtx.java
@@ -0,0 +1,28 @@
+// Solution to 69. Sqrt(x)
+// Problem: https://leetcode.com/problems/sqrtx/
+// Author: Sumit Gupta
+
+public class Solution {
+	public int mySqrt(int x) {
+		if (x <= 1)
+			return x;
+		int left = 0, right = x;
+
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+
+			if (mid == x / mid)
+				return mid;
+
+			if (mid < x / mid) {
+				if (mid + 1 > x / (mid + 1))
+					return mid;
+				left = mid + 1;
+			}
+			else
+				right = mid - 1;
+		}
+
+		return left;
+	}
+}
