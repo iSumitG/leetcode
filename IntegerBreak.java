c75cbed0a4fcee45331dba43227a9e664c09d379
Sumit Gupta <sumit743@gmail.com>
2016-08-14 18:04:58 -0400

    Update IntegerBreak.java

--- IntegerBreak.java
+++ IntegerBreak.java
@@ -4,21 +4,16 @@
 
 public class Solution {
     public int integerBreak(int n) {
-        
         if(n == 2) return 1;
         if(n == 3) return 2;
         
         int pro = 1;
-        
-        while(n>4)
-        {
+        while(n>4) {
             n = n-3;
             pro = pro*3;
         }
         
         pro = pro * n;
-        
         return pro;
     }
-    
 }
