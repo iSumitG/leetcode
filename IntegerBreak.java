e552fd28ccff298c495d913a152a2ef758164a09
Sumit Gupta <sumit743@gmail.com>
2016-08-14 18:04:27 -0400

    Update IntegerBreak.java

--- IntegerBreak.java
+++ IntegerBreak.java
@@ -2,3 +2,23 @@
 // Problem: https://leetcode.com/problems/integer-break/
 // Author: Sumit Gupta
 
+public class Solution {
+    public int integerBreak(int n) {
+        
+        if(n == 2) return 1;
+        if(n == 3) return 2;
+        
+        int pro = 1;
+        
+        while(n>4)
+        {
+            n = n-3;
+            pro = pro*3;
+        }
+        
+        pro = pro * n;
+        
+        return pro;
+    }
+    
+}
