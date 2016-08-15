57b34c0cc31ef2573246a610a35080f2fb75db5c
Sumit Gupta <sumit743@gmail.com>
2016-08-15 18:31:15 -0400

    Update CountingBits.java

--- CountingBits.java
+++ CountingBits.java
@@ -2,5 +2,16 @@
 // Problem: https://leetcode.com/problems/counting-bits/
 // Author: Sumit Gupta
 
-
+public class Solution {
+    public int[] countBits(int num) {
+        int[] a = new int[num+1];
+        for(int i=0; i<=num; i++) {
+            if(i%2 == 0) //even
+                a[i] = a[i/2];
+            else //odd
+                a[i] = a[i/2] + 1;
+        }
+        return a;
+    }
+}
 
