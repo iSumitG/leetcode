4819e9e209b1a63cb95a315751b7f3c7e5aa970c
Sumit Gupta <sumit743@gmail.com>
2016-08-16 18:09:17 -0400

    Update MaximumProductOfWordLengths.java

--- MaximumProductOfWordLengths.java
+++ MaximumProductOfWordLengths.java
@@ -16,15 +16,9 @@ public class Solution {
         
         int max = 0;
         for(int i=0; i<words.length; i++)
-        {
             for(int j=i+1; j<words.length; j++)
-            {
                 if((bitval[i] & bitval[j]) == 0)
-                {
                     max = Math.max(max, words[i].length() * words[j].length());
-                }
-            }
-        }
         return max;
     }
 }
