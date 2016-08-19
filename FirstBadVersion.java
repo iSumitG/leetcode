f6c1c017e494b0a22fa8c943d04f1a0abf14291e
Sumit Gupta <sumit743@gmail.com>
2016-08-19 18:19:47 -0400

    Update FirstBadVersion.java

--- FirstBadVersion.java
+++ FirstBadVersion.java
@@ -9,20 +9,14 @@ public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         if(n==0 || n==1) return n;
         
-        int start = 1;
-        int end = n;
+        int start = 1, end = n;
         
-        while(start<end)
-        {
+        while(start < end) {
             int mid = (start/2)+(end/2);
             if(isBadVersion(mid))
-            {
                 end = mid;
-            }
             else
-            {
                 start = mid+1;
-            }
         }
         return start;
     }
