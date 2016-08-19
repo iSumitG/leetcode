81bf057749313b2debf087d4656f741c4397e21a
Sumit Gupta <sumit743@gmail.com>
2016-08-18 21:53:03 -0400

    Update WordPattern.java

--- WordPattern.java
+++ WordPattern.java
@@ -2,5 +2,37 @@
 // Problem: https://leetcode.com/problems/word-pattern/
 // Author: Sumit Gupta
 
-
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        
+        if(pattern == null || str == null) return false;
+        
+        String[] strArray = str.split(" ");
+        
+        if(pattern.length() != strArray.length) return false;
+        
+        HashMap<Character, String> hm = new HashMap<Character, String>();
+        
+        for(int i=0; i<pattern.length(); i++)
+        {
+            char c = pattern.charAt(i);
+            if(hm.containsKey(c))
+            {
+                String word = hm.get(c);
+                if(!word.equals(strArray[i])) return false;
+            }
+            else
+            {
+                if(hm.containsValue(strArray[i]))
+                {
+                    return false;
+                }
+                hm.put(c, strArray[i]);
+            }
+        }
+        
+        return true;
+        
+    }
+}
 
