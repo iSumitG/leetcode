e29715b466d84df612cbda6bbdb9628552823c07
Sumit Gupta <sumit743@gmail.com>
2017-03-11 14:29:48 -0500

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -3,15 +3,14 @@
 // Author: Sumit Gupta
 
 public class Solution {
-	public boolean isPalindrome(int n) {
-        if (n < 0 || (n != 0 && n % 10 == 0)) return false;
-        int rev = 0;
-        
-        while (n > rev) {
-        	rev = rev * 10 + n % 10;
-    	    n = n / 10;
-        }
-        
-        return (n == rev || n == rev/10);
-    }
+    public boolean isPalindrome(int n) {
+		int temp = n, rev = 0;
+
+		while (temp > 0) {
+			rev = 10 * rev + temp % 10;
+			temp /= 10;
+		}
+
+		return rev == n;
+	}
 }
